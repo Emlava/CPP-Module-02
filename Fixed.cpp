95f367d6d308cce6a781953b74d6f19480bfea99
Emilio Lara <emiliolaravargas@hotmail.com>
2026-07-05 20:43:37 +0200

    Finished ex00.

---
+++ Fixed.cpp
@@ -0,0 +1,44 @@
+#include "Fixed.hpp"
+
+
+// Member functions needed to have canonical form -->
+
+Fixed::Fixed() : value(0) // Default constructor
+{
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed& instance) // Copy Constructor
+{
+	std::cout << "Copy constructor called\n";
+	*this = instance;
+}
+
+Fixed&	Fixed::operator=(const Fixed& rhs_instance) // Copy assignment operator overload
+{
+	std::cout << "Copy assignment operator called\n";
+	if (this != &rhs_instance)
+		value = rhs_instance.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed(void) // Destructor
+{
+	std::cout << "Destructor called\n";
+}
+
+// <--
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits() member function called\n";
+	return (this->value);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	this->value = raw;
+	return ;
+}
+
+
--- Fixed.hpp
+++ Fixed.hpp
@@ -8,15 +8,15 @@ class Fixed
 	public:
 		Fixed(void); // Default constructor
 		Fixed(const Fixed& instance); // Copy constructor
-		Fixed	operator=(const Fixed& instance); // Copy assignment operator overload
-		~Fixed();
+		Fixed&	operator=(const Fixed& rhs_instance); // Copy assignment operator overload
+		~Fixed(void); // Destructor
 
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 
 	private:
 		int		value;
-		static const int	fraction_bits = 8;
+		static const int	fraction_bits = 8; // Non-member attribute
 };
 
 #endif
